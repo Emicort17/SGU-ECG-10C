28116df807702ed3aaeed5e707fe269031a55143
Emicort17 <133397347+Emicort17@users.noreply.github.com>
2025-11-17 17:35:12 -0600

    Change API_URL to use HTTPS

--- front-ECG-10C/src/models/server.controller.js
+++ front-ECG-10C/src/models/server.controller.js
@@ -1,7 +1,7 @@
 const ServerController = {};
 const ENV = import.meta.env;
 
-const API_URL = `http://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`
+const API_URL = `https://${ENV.VITE_API_HOST}:${ENV.VITE_API_PORT}${ENV.VITE_API_BASE}`
 
 async function handleResponse(response) {
     if (!response) return null;
